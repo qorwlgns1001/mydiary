474902c95a98bd672aae4fe0a065d40d14a6a998
dojes <bjh59629@naver.com>
2024-01-30 00:30:06 +0900

    @Autowired 의존 주입 및 primary ID 설정

--- src/main/java/com/example/logindemo/controller/MemberController.java
+++ src/main/java/com/example/logindemo/controller/MemberController.java
@@ -1,31 +1,39 @@
 package com.example.logindemo.controller;
 
-import com.example.logindemo.dao.MemberDao;
 import com.example.logindemo.member.Member;
+import com.example.logindemo.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MemberController {
-    private MemberDao memberDao = new MemberDao();
+    private final MemberRepository memberRepository;
     private Member member;
+
+    @Autowired
+    public MemberController(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
 
     // ID로 특정 유저 검색
     @GetMapping("/member/{id}")
-    public Member findMember(@PathVariable("id") Long id) {
-        return memberDao.findById(id);
+    public Optional<Member> findMember(@PathVariable("id") Long id) {
+        return memberRepository.findById(id);
     }
 
     @GetMapping("/member/all")
     public List<Member> findAll() {
-        return memberDao.findAll();
+        return memberRepository.findAll();
     }
 
     @PutMapping("/member/put")
     public void saveMember(@RequestParam String loginId, @RequestParam String password, @RequestParam String name, @RequestParam int age) throws Exception {
-        //member = new Member(loginId, password, name, age);
-        memberDao.saveMember(member);
+        member = new Member(loginId, password, name, age, LocalDateTime.now());
+        memberRepository.save(member);
     }
 }
--- src/main/java/com/example/logindemo/member/Member.java
+++ src/main/java/com/example/logindemo/member/Member.java
@@ -1,32 +1,35 @@
 package com.example.logindemo.member;
 
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@NoArgsConstructor
 @RequiredArgsConstructor
 @Entity
 @Table(name = "MEMBER")
 public class Member {
     @Id
     @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long Id; // 고유 키
+    @NonNull
     @Column(name = "LOGINID", nullable = false)
     private String loginId; // 유저 아이디
+    @NonNull
     @Column(name = "PASSWORD", nullable = false)
     private String password; // 유저 패스워드
+    @NonNull
     @Column(name = "NAME", nullable = false)
     private String name; // 유저 이름
+    @NonNull
     @Column(name = "AGE", nullable = false)
     private int age; // 유저 나이
+    @NonNull
     @Column(name = "CREATETIME")
     private LocalDateTime createTime; // 생성 시간
 }
